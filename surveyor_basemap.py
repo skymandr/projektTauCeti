a647bec682f52f3402cb991eb05d324fddf284b2
Andreas Skyman <skymandr@fripost.org>
2013-02-06 21:51:49 +0100

    implemented modt of surveyor_basemap

--- surveyor_basemap.py
+++ surveyor_basemap.py
@@ -1,33 +1,22 @@
 #! /usr/bin/env python
 import os
 import sys
-import projekt_anarres as p
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons, Cursor
 from mpl_toolkits.basemap import Basemap
-from testgrids import make_testgrids as grids
 
 
 # TODO:
-#   - conversion to basemap
-#   - remove mode
 #   - make radiobuttons for the following projections:
 #       * orthographic
 #       * azimuthal equidistant
 #       * equirectangular
-#   - implement the above projections
 
 
 class PlanetarySurveyor(object):
     def __init__(self, filename):
         self.filename = filename
-
-        map_image = plt.imread(filename)
-        while len(map_image.shape) > 2:
-            map_image = map_image.mean(-1)
-
-        self.map_image = map_image
 
         # Setup display:
         self.fig = plt.figure(1)
@@ -35,39 +24,37 @@ class PlanetarySurveyor(object):
         plt.clf()
         plt.subplots_adjust(left=0.1, bottom=0.20)
 
-        self.meridian = 90
-        self.parallel = 90
-        self.mode = "azimuthal"
+        self.meridian = -90
+        self.parallel = 22.5
         self.projection = "orthographic"
+        self.parallels = 16
+        self.meridians = 16
 
         self.setup_display()
 
         # Setup mouse interaction:
-        self.click = self.display.figure.canvas.mpl_connect(
+        self.click = self.hemisphere_axes.figure.canvas.mpl_connect(
                         'button_press_event', self.mouseclick)
 
-        self.cursor = Cursor(self.display.axes, useblit=True, color='red',
+        self.cursor = Cursor(self.hemisphere_axes, useblit=True, color='red',
                              linewidth=1)
 
         # Setup axes:
         self.axes_step = plt.axes([0.15, 0.15, 0.60, 0.03])
         self.axes_meridians = plt.axes([0.15, 0.10, 0.60, 0.03])
         self.axes_parallels = plt.axes([0.15, 0.05, 0.60, 0.03])
-        self.reset_axes = plt.axes([0.82, 0.05, 0.15, 0.04])
-        self.coord_axes = plt.axes([0.82, 0.14, 0.15, 0.04])
+        self.reset_axes = plt.axes([0.84, 0.05, 0.15, 0.04])
+        self.coord_axes = plt.axes([0.84, 0.14, 0.15, 0.04])
 
         # Setup sliders:
         self.step = 22.5
-        self.parallels = 0
-        self.meridians = 0
-
         self.slider_step = Slider(self.axes_step, 'Step', 0, 90,
                                   valinit=self.step, valfmt='%2.1f')
         self.slider_meridians = Slider(self.axes_meridians, 'Meridians', 0,
-                                       42, valinit=self.parallels,
+                                       64, valinit=self.parallels,
                                        valfmt='%2d')
         self.slider_parallels = Slider(self.axes_parallels, 'Parallels', 0,
-                                       42, valinit=self.parallels,
+                                       64, valinit=self.parallels,
                                        valfmt='%2d')
 
         self.slider_step.on_changed(self.update)
@@ -84,47 +71,66 @@ class PlanetarySurveyor(object):
         plt.show()
 
     def setup_display(self):
-        self.R = 180
-        self.padding = self.R / 10
-        if self.mode == 'azimuthal':
-            self.hemisphere = p.get_azimuthal_hemisphere(
-                                self.map_image, self.meridian, self.parallel,
-                                self.R, self.projection, self.padding)
-            self.display = plt.imshow(self.hemisphere, cmap=plt.cm.gray,
-                                      extent=[-1.5, 1.5, -1.5, 1.5])
-            plt.axis([-1.5, 1.5, -1.5, 1.5])
-            plt.axis('off')
-        elif self.mode == 'rectangular':
-            pass
+        self.hemisphere_axes = plt.gca()
+        self.R = 10000 * 360.0 / (2 * np.pi)
+        self.width = 180 * 10000
+        self.height = 180 * 10000
+
+        if self.projection == 'orthographic':
+            self.hemisphere = Basemap(projection="ortho", lon_0=self.meridian,
+                              lat_0=self.parallel, resolution='c',
+                              area_thresh=100000, ax=self.hemisphere_axes)
+        elif self.projection == 'equidistant':
+            self.hemisphere = Basemap(projection="aeqd", lon_0=self.meridian,
+                              lat_0=self.parallel, resolution='c',
+                              area_thresh=100000, rsphere=self.R,
+                              ax=self.hemisphere_axes, width=self.width,
+                              height=self.height)
+        elif self.projection == 'rectangular':
+            self.hemisphere = Basemap(projection="cyl", lon_0=0, lat_0=0,
+                              resolution='c', area_thresh=100000,
+                              ax=self.hemisphere_axes)
+
+        self.hemisphere.warpimage(self.filename)
+        self.hemisphere.drawmapboundary()
+        self.draw_graticules()
 
     def update_display(self):
-        if self.mode == 'azimuthal':
-            self.hemisphere = p.get_azimuthal_hemisphere(
-                                self.map_image, self.meridian, self.parallel,
-                                self.R, self.projection, self.padding)
-            ax = self.display.axes
-            self.display = ax.imshow(self.hemisphere, cmap=plt.cm.gray,
-                                      extent=[-1.5, 1.5, -1.5, 1.5])
-            plt.axis([-1.5, 1.5, -1.5, 1.5])
-            plt.axis('off')
-        elif self.mode == 'rectangular':
-            pass
+        if self.projection == 'orthographic':
+            self.hemisphere = Basemap(projection="ortho", lon_0=self.meridian,
+                              lat_0=self.parallel, resolution='c',
+                              area_thresh=100000, ax=self.hemisphere_axes)
+        elif self.projection == 'equidistant':
+            self.hemisphere = Basemap(projection="aeqd", lon_0=self.meridian,
+                              lat_0=self.parallel, resolution='c',
+                              area_thresh=100000, rsphere=self.R,
+                              ax=self.hemisphere_axes, width=self.width,
+                              height=self.height)
+        elif self.projection == 'rectangular':
+            self.hemisphere = Basemap(projection="cyl", lon_0=0, lat_0=0,
+                              resolution='c', area_thresh=100000,
+                              ax=self.hemisphere_axes)
+
+        self.hemisphere_axes.cla()
+        self.hemisphere.warpimage(self.filename)
+        self.hemisphere.drawmapboundary()
+        self.draw_graticules()
 
     def update(self, val):
         if self.step != self.slider_step.val:
             self.step = np.round(self.slider_step.val / 0.5) * 0.5
             self.slider_step.set_val(self.step)
 
-        self.meridians = np.int(self.slider_meridians.val)
-        self.parallels = np.int(self.slider_parallels.val)
+        if (self.meridians != self.slider_meridians.val or
+                self.parallels != self.slider_parallels.val):
 
-        self.update_display()
+            self.meridians = np.int(self.slider_meridians.val)
+            self.parallels = np.int(self.slider_parallels.val)
 
-        if self.meridians > 0 or self.parallels > 0:
-            self.draw_graticules()
+            self.update_display()
 
-        #print self.get_coordinates()
-        #print self.get_graticule()
+            if self.meridians > 0 or self.parallels > 0:
+                self.draw_graticules()
 
         plt.draw()
 
@@ -133,30 +139,61 @@ class PlanetarySurveyor(object):
         self.slider_meridians.reset()
         self.slider_parallels.reset()
         self.meridian = 90
-        self.parallel = 90
+        self.parallel = 0
+        self.update(0)
 
     def mouseclick(self, event):
-        if event.inaxes == self.display.axes:
+        if event.inaxes == self.hemisphere_axes:
             if event.button == 1:
-                x, y = np.round(event.xdata), np.round(event.ydata)
-                if self.mode == "azimuthal":
-                    self.meridian += self.step * x
-                    #self.parallel -= self.step * y
-                elif self.mode == "rectangular":
-                    self.meridian = x
-                    self.parallel = -y
+                if self.projection == "rectangular":
+                    self.parallel = np.round(event.ydata / 0.5) * 0.5
+                    self.meridian = np.round(event.xdata / 0.5) * 0.5
+                else:
+                    xlim = self.hemisphere_axes.get_xlim()
+                    x = np.round(3 * (event.xdata -
+                        0.5 * (xlim[1] - xlim[0])) / (xlim[1] - xlim[0]))
 
-                self.meridian %= 360.0
-                self.parallel %= 360.0
-                self.coords.label.set_text("{0}".format(
-                                           self.get_coordinates()))
+                    ylim = self.hemisphere_axes.get_ylim()
+                    y = np.round(3 * (event.ydata -
+                        0.5 * (ylim[1] - ylim[0])) / (ylim[1] - ylim[0]))
+
+                    self.meridian += self.step * x
+                    self.parallel += self.step * y
+
+                    self.update_display()
+
+                self.fix_coordinates()
+
                 self.update(0)
 
-    def get_coordinates(self):
-        parallel = self.parallel
-        meridian = self.meridian
+    def fix_coordinates(self):
+            if self.parallel > 90.0:
+                self.parallel = 180.0 - self.parallel
+            elif self.parallel < -90.0:
+                self.parallel = -180.0 - self.parallel
 
-        return parallel, meridian
+            if self.meridian > 180.0:
+                self.meridian = 360.0 - self.meridian
+            elif self.meridian < -180.0:
+                self.meridian = -360.0 - self.meridian
+
+            self.coords.label.set_text("{0}".format(self.get_coordinates()))
+
+    def get_coordinates(self):
+        parallel = np.abs(self.parallel)
+        meridian = np.abs(self.meridian)
+
+        if self.parallel >= 0:
+            NS = "N"
+        else:
+            NS = "S"
+
+        if self.meridian >= 0:
+            EW = "E"
+        else:
+            EW = "W"
+
+        return "{0} {1}, {2} {3}".format(parallel, NS, meridian, EW)
 
     def get_graticule(self):
         try:
@@ -168,11 +205,26 @@ class PlanetarySurveyor(object):
             return None, None
 
     def draw_graticules(self):
-        pass
+        parallel_step = 180.0 / self.parallels
+        meridian_step = 360.0 / self.meridians
+
+        if self.parallels > 0:
+            min_parallel = -90
+            max_parallel = 90 - parallel_step
+            self.hemisphere.drawparallels(
+                            np.arange(min_parallel, max_parallel + 1,
+                            parallel_step), latmax=90 - parallel_step)
+
+        if self.meridians > 0:
+            min_meridian = -180
+            max_meridian = 180 - meridian_step
+            self.hemisphere.drawmeridians(
+                            np.arange(min_meridian, max_meridian + 1,
+                            meridian_step), latmax=90 - parallel_step)
 
 
 def main():
-    filename = 'new/anarres_small_eq_2_grey.png'
+    filename = 'new/anarres_small_eq_2.png'
     print filename
     Surveyor = PlanetarySurveyor(filename)
 
